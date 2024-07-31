3ddd2a842c24929a848c7672f0c7b9d2d206fbfd
Fabian Stoehr <stoehr@ub.uni-leipzig.de>
2024-07-31 10:47:20 +0200

    test:local mirador image tools v4

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './dist/',
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "./dist/",
   },
-};
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 9000,
+  },
+}
